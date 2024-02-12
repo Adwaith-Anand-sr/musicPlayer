c2e2e3c16ddecf735cfca8bf3891afcc7a88b6b6
Adwaith-Anand-sr <139732933+Adwaith-Anand-sr@users.noreply.github.com>
2024-02-12 13:17:23 +0530

    update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ let backgroundImage = document.querySelector(".imageDisplay")
 backgroundImage.style.backgroundImage=`url('Images/images (1).jpeg')`
 
 let audio = document.querySelector(".audio")
-audio.src = "Music/96_Songs_The_Life_of_Ram_Video_Song_Vijay_Sethupathi_Tr_6LD30ChPsSs.mp3"
+console.log(audio);

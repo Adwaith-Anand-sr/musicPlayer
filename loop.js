fd04f30ed57dd617076a46d8466fec59e40aeadf
Adwaith-Anand-sr <139732933+Adwaith-Anand-sr@users.noreply.github.com>
2024-02-12 15:09:21 +0530

    update loop.js

--- loop.js
+++ loop.js
@@ -1,26 +1,23 @@
 const owner = 'adwaith-anand-sr';
 const repo = 'musicPlayer';
 const path = 'Music';
-let sourceArray = []
+
 let index = 0
+// looping through Music folder and getting names of each files
 async function loopThroughFolder() {
    
   try {
     const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`);
     const data = await response.json();
-
     for (const item of data) {
        if (!sourceArray[index]) {
         sourceArray[index] = [];
       }
       sourceArray[index].push(item.name);
       index++;
-      
-      console.log(item.name);
     }
   } catch (error) {
     console.error('Error fetching folder contents:', error);
   }
 }
-
 loopThroughFolder();

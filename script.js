34edb2407181c4cb98ca339884691b5b4126209d
Adwaith-Anand-sr <139732933+Adwaith-Anand-sr@users.noreply.github.com>
2024-02-13 07:16:27 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 let backgroundImages = document.querySelector(".imageDisplay")
 backgroundImages.style.backgroundImage = `url('Images/images (1).jpeg')`
+
+let list= document.querySelector(".list")
+console.log(sourceArray);
+sourceArray.forEach((item,i) =>{
+   console.log("list");
+   list.innerHTML += `
+      <div class="list">
+         <div class ="elem">
+            <div class="img"></div>
+            <h3>${item}</h3>
+         </div>
+      </div>
+   `
+   console.log(list);
+   console.log("list");
+})

414740a0fabb70864871473d88d8ccb4c422b5a6
Adwaith-Anand-sr <139732933+Adwaith-Anand-sr@users.noreply.github.com>
2024-02-13 07:35:28 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 let backgroundImages = document.querySelector(".imageDisplay")
 backgroundImages.style.backgroundImage = `url('Images/images (1).jpeg')`
+let sourceArray = []
+let song = document.querySelector("audio")
 
-let list= document.querySelector(".list")
-console.log(sourceArray);
-sourceArray.forEach((item,i) =>{
-   console.log("list");
-   list.innerHTML += `
-      <div class="list">
-         <div class ="elem">
-            <div class="img"></div>
-            <h3>${item}</h3>
-         </div>
-      </div>
-   `
-   console.log(list);
-   console.log("list");
-})
+
+function loop() {
+   // looping through Music folder and getting names of each files
+
+   const owner = 'adwaith-anand-sr';
+   const repo = 'musicPlayer';
+   const path = 'Music';
+   
+   async function loopThroughFolder() {
+      
+     try {
+       const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`);
+       const data = await response.json();
+       for (const item of data) {
+         sourceArray.push("Music/"+item.name);
+       }
+     } catch (error) {
+       console.error('Error fetching folder contents:', error);
+     }
+   }
+   loopThroughFolder();
+
+}
+loop()
+
+console.log(sourceArray[1]);
